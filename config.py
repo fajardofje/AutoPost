81f88fcc1b0af010864d51541074620ab4ec317f
fajardofje <fajardofje@gmail.com>
2024-09-03 09:28:21 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+ACCESS_TOKEN = "Your Credential here"
+ACCESS_TOKEN_SECRET = "Your Credential here"
+
+BAERER_TOKEN = "Your Credential here"
+
+CONSUMER_KEY = "Your Credential here"
+CONSUMER_SECRET = "Your Credential here"
